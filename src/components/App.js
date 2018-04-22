6496d246771801b351134f1cc6c36f60a4cebcb6
Chris <nothanks@nowhere>
2018-04-22 20:51:05 +0100

    bjklhb

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,20 @@
-import React from 'react';
-import Interactive from 'react-interactive';
-import { Switch, Route } from 'react-router-dom';
-import Home from './Home';
-import ExampleComponent from './ExampleComponent';
-import PageNotFound from './PageNotFound';
-import Breadcrumbs from './Breadcrumbs';
-import s from '../styles/app.style';
+import React from 'react';import Interactive from 'react-interactive';import { Switch, Route } from 'react-router-dom';import Home from './Home';
+import ExampleComponent from './ExampleComponent';import PageNotFound from './PageNotFound';import Breadcrumbs from './Breadcrumbs';import s from '../styles/app.style';
 
-export default function App() {
-  return (
-    <div style={s.root}>
-      <h1 style={s.title}>Single Page Apps for GitHub Pages</h1>
-      <Interactive
+export default function App() {return (<div style={s.root}><h1 style={s.title}>Single Page Apps for GitHub Pages</h1><Interactive
         as="a"
         href="https://github.com/rafrex/spa-github-pages"
         style={s.repoLink}
         {...s.link}
       >https://github.com/rafrex/spa-github-pages</Interactive>
 
-      <nav style={s.breadcrumbs}>
-        <Breadcrumbs />
-      </nav>
+      <nav style={s.breadcrumbs}><Breadcrumbs /></nav>
 
-      <Switch>
-        <Route exact path="/" component={Home} />
+      <Switch><Route exact path="/" component={Home} />
         <Route path="/example" component={ExampleComponent} />
-        <Route component={PageNotFound} />
-      </Switch>
+        <Route component={PageNotFound} /></Switch>
 
-      <div style={s.creditLine}>
-        <Interactive
+      <div style={s.creditLine}><Interactive
           as="a"
           href="http://www.rafaelpedicini.com"
           interactiveChild
@@ -37,9 +22,4 @@ export default function App() {
           touchActive={{}}
           touchActiveTapOnly
         >
-          Code and concept by <span {...s.childLink}>Rafael Pedicini</span>
-        </Interactive>
-      </div>
-    </div>
-  );
-}
+          Code and concept by <span {...s.childLink}>Rafael Pedicini</span></Interactive></div></div>);}
