8e17255a93411b0051ddcb9edd9fc1d3ba90fe7c
Chris <nothanks@nowhere>
2018-04-22 21:15:43 +0100

    hfxhfd

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,19 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Code } from '../styles/style';
-import s from '../styles/pageNotFound.style';
+import React from 'react';import PropTypes from 'prop-types';import { Code } from '../styles/style';import s from '../styles/pageNotFound.style';
 
-const propTypes = {
-  location: PropTypes.object.isRequired,
-};
+const propTypes = {location: PropTypes.object.isRequired,};
 
 export default function PageNotFound({ location }) {
-  return (
-    <p style={s.p}>
-      Page not found - the path, <Code>{location.pathname}</Code>,
-      did not match any React Router routes.
-    </p>
-  );
-}
+  return (<p style={s.p}> Page not found - the path, <Code>{location.pathname}</Code>,  did not match any React Router routes.</p>);}
 
 PageNotFound.propTypes = propTypes;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
-ReactDOM.render(
-  <BrowserRouter>
+ReactDOM.render(<BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root'),
-);
+  </BrowserRouter>, document.getElementById('root'), );
