d497e63630000dc1c6552b7a06375194fceb74ff
Chris <nothanks@nowhere>
2018-04-23 11:02:30 +0100

    ;kjn

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,16 @@
 import React from 'react';import Interactive from 'react-interactive';import { Switch, Route } from 'react-router-dom';import Home from './Home';
-import ExampleComponent from './ExampleComponent';import PageNotFound from './PageNotFound';import Breadcrumbs from './Breadcrumbs';import s from '../styles/app.style';
+import PageNotFound from './PageNotFound';import Breadcrumbs from './Breadcrumbs';import s from '../styles/app.style';
+import ExampleComponent from './ExampleComponent';import Example404forspaComponent from './Example404forspaComponent';
 
-export default function App() {return (<div style={s.root}><h1 style={s.title}>Single Page Apps for GitHub Pages</h1><Interactive
-        as="a"
-        href="https://github.com/rafrex/spa-github-pages"
-        style={s.repoLink}
-        {...s.link}
-      >https://github.com/rafrex/spa-github-pages</Interactive>
+export default function App() {return (<div style={s.root}><h1 style={s.title}>Single Page Apps for GitHub Pages</h1>
+  <Interactive as="a" href="https://github.com/rafrex/spa-github-pages" style={s.repoLink} {...s.link}  >https://github.com/rafrex/spa-github-pages</Interactive>
 
-      <nav style={s.breadcrumbs}><Breadcrumbs /></nav>
+    <nav style={s.breadcrumbs}><Breadcrumbs /></nav>
 
-      <Switch><Route exact path="/" component={Home} />
-        <Route path="/example" component={ExampleComponent} />
-        <Route component={PageNotFound} /></Switch>
+    <Switch><Route exact path="/" component={Home} />
+      <Route path="/example" component={ExampleComponent} />
+      <Route path="/example404forspa" component={Example404forspaComponent} />
+      <Route component={PageNotFound} /></Switch>
 
-      <div style={s.creditLine}><Interactive
-          as="a"
-          href="http://www.rafaelpedicini.com"
-          interactiveChild
-          focus={{}}
-          touchActive={{}}
-          touchActiveTapOnly
-        >
-          Code and concept by <span {...s.childLink}>Rafael Pedicini</span></Interactive></div></div>);}
+    <div style={s.creditLine}><Interactive as="a" href="http://www.rafaelpedicini.com" interactiveChild  focus={{}}  touchActive={{}} touchActiveTapOnly>
+        App.js comment:Code and concept by <span {...s.childLink}>Rafael Pedicini</span></Interactive></div></div>);}
